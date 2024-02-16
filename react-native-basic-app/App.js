f8f1242884749c1dff9ec755e97c1b969d572b40
jhsonnn <jihyeongson13@gmail.com>
2024-02-16 11:59:52 +0900

    Core Components 이용한 UI 작성

--- react-native-basic-app/App.js
+++ react-native-basic-app/App.js
@@ -1,12 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './components/screens/MainScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! 안녕하세요!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MainScreen />
   );
 }
 
@@ -18,8 +15,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
-
-///Users/jih/Library/Android/sdk
-//export ANDROID_HOME=/Users/jih/Library/Android/sdk && export PATH=$PATH:$ANDROID_HOME/emulator && export PATH=$PATH:$ANDROID_HOME/platform-tools
--- react-native-basic-app/components/screens/MainScreen.js
+++ react-native-basic-app/components/screens/MainScreen.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const MainScreen = () => {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+    <SafeAreaView>
+        <StatusBar barStyle={'default'} />
+        <Text>ToDo App</Text>
+        <View>
+            <Text>할 일</Text>
+        </View>
+        <View>
+            <Text>완료된 일</Text>
+        </View>
+    </SafeAreaView>
   )
 }
 
 export default MainScreen
+
+const styles = StyleSheet.create({})
