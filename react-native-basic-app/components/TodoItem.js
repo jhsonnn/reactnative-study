53bf9ab4a4e5858714f3e8daf331198560498143
jhsonnn <jihyeongson13@gmail.com>
2024-02-16 15:01:57 +0900

    SVG 사용하기
    체크박스, delete button 넣고 스타일링

--- react-native-basic-app/components/TodoItem.js
+++ react-native-basic-app/components/TodoItem.js
@@ -1,13 +1,17 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import CheckboxUnchecked from '../assets/checkbox-unchecked.svg';
+import CheckboxChecked from '../assets/checkbox-checked.svg';
+import DeleteIcon from '../assets/delete.svg'
 
 const TodoItem = () => {
   return (
     <View style={styles.itemContainer}>
         <Pressable
-            style={styles.itemTextChecked}
+            style={styles.itemCheckbox}
         >
-           
+            <CheckboxUnchecked />
+            <CheckboxChecked style={styles.itemCheckboxCheckedIcon} />
         </Pressable>
         <Text
              style={[styles.itemText, styles.itemTextChecked]}
@@ -21,6 +25,7 @@ const TodoItem = () => {
             ]}
             hitSlop={10}
         >
+            <DeleteIcon />
         </Pressable>
     </View>
   )
@@ -30,6 +35,46 @@ export default TodoItem
 
 const styles = StyleSheet.create({
     itemContainer: {
-
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#f7f8fa'
+    },
+    itemCheckbox: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 20,
+        height: 20,
+        marginRight: 13,
+        borderRadius: 6
+    },
+    itemCheckboxCheckedIcon: {
+        shadowColor: '#000000',
+        shadowOpacity: 0.14,
+        shadowRadius: 8,
+        shadowOffset: {
+            width: 0,
+            height: 4
+        }
+    },
+    itemText: {
+        marginRight: 'auto',
+        paddingRight: 25,
+        fontSize: 15,
+        lineHeight: 20,
+        color: '#737373',
+    },
+    itemTextChecked: {
+        opacity: 0.3,
+        textDecorationLine: 'line-through',
+    },
+    deleteButton: {
+        opacity: 0.89
+    },
+    deleteButtonDone: {
+        opacity: 0.3
     }
+
 })
