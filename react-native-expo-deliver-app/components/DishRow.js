7ad34af0e7577266907c16f4be599835837bda9f
jhsonnn <jihyeongson13@gmail.com>
2024-03-08 16:17:48 +0900

    Dish Row 데이터 생성하기

--- react-native-expo-deliver-app/components/DishRow.js
+++ react-native-expo-deliver-app/components/DishRow.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToBasket,
+  clearBasket,
+  removeFromBasket,
+  selectBasketItemWithId,
+} from "../store/basketSlice";
 
-const DishRow = () => {
+const DishRow = ({ id, name, description, price, image }) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const items = useSelector((state) => selectBasketItemWithId(state, id));
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+    dispatch(removeFromBasket({ id }));
+  };
+
+  const clearItemsFromBasket = () => {
+    dispatch(clearBasket());
+  };
+
   return <Text>DishRow</Text>;
 };
 
