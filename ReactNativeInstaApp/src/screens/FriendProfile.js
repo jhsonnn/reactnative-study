8ed7c9d9d57c6d325c419cf85d5ec474ecaaa190
jhsonnn <jihyeongson13@gmail.com>
2024-03-12 03:03:24 +0900

    인스타 앱 전체 구조 생성하기

---
+++ ReactNativeInstaApp/src/screens/FriendProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const FriendProfile = () => {
+  return (
+    <View>
+      <Text>FriendProfile</Text>
+    </View>
+  );
+};
+
+export default FriendProfile;
