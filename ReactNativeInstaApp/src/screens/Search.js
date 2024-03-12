74868081de8e5dc4433c92d5b22c22eeffa717ee
jhsonnn <jihyeongson13@gmail.com>
2024-03-12 17:08:46 +0900

    Search Content UI(1)

--- ReactNativeInstaApp/src/screens/Search.js
+++ ReactNativeInstaApp/src/screens/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import SearchInput from '../SearchInput';
+import SearchContent from '../components/SearchContent';
 
 const Search = () => {
   return (
@@ -9,7 +10,7 @@ const Search = () => {
       style={{width: '100%', backgroundColor: 'white', position: 'relative'}}>
       <ScrollView>
         <SearchInput />
-        {/* SearchContent */}
+        <SearchContent />
       </ScrollView>
     </SafeAreaView>
   );
