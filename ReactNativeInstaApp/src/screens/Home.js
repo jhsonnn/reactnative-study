7c090d839f58adc973b669c45c999c9ce921313b
jhsonnn <jihyeongson13@gmail.com>
2024-03-12 09:52:38 +0900

    HomeScreen UI 생성하기

--- ReactNativeInstaApp/src/screens/Home.js
+++ ReactNativeInstaApp/src/screens/Home.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ScrollView, StatusBar, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <View
+        style={{
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+          paddingHorizontal: 15,
+          alignItems: 'center',
+        }}>
+        <View>
+          <Text style={{fontSize: 25, fontWeight: '500'}}>Instagram</Text>
+        </View>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <FontAwesome
+            name="plus-square-o"
+            style={{fontSize: 24, paddingHorizontal: 15}}
+          />
+          <Feather name="navigation" style={{fontSize: 24}} />
+        </View>
+      </View>
+      <ScrollView>
+        {/* Stories */}
+        {/* Posts */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
