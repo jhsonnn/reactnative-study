43e64576f693316ea07cf3e1e66ae93beea003ef
jhsonnn <jihyeongson13@gmail.com>
2024-03-12 17:52:10 +0900

    Search Content UI(2)

--- ReactNativeInstaApp/src/components/SearchContent.js
+++ ReactNativeInstaApp/src/components/SearchContent.js
@@ -68,6 +68,77 @@ const SearchContent = () => {
                 })}
               </View>
             ) : null}
+            {data.id === 1 ? (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    width: '66.5%',
+                    justifyContent: 'space-between',
+                  }}>
+                  {data.images.slice(0, 4).map((imageData, imgIndex) => {
+                    return (
+                      <TouchableOpacity
+                        key={imgIndex}
+                        style={{paddingBottom: 2, width: '49.5%'}}>
+                        <Image
+                          source={imageData}
+                          style={{width: '100%', height: 150}}
+                        />
+                      </TouchableOpacity>
+                    );
+                  })}
+                </View>
+                <TouchableOpacity style={{marginLeft: 2, width: '33%'}}>
+                  <Image
+                    source={data.images[4]}
+                    style={{width: '100%', height: 300}}
+                  />
+                </TouchableOpacity>
+              </View>
+            ) : null}
+            {data.id === 2 ? (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                }}>
+                <TouchableOpacity style={{paddingRight: 2, width: '66.5%'}}>
+                  <Image
+                    source={data.images[2]}
+                    style={{width: '100%', height: 300}}
+                  />
+                </TouchableOpacity>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    width: '33%',
+                    justifyContent: 'space-between',
+                  }}>
+                  {data.images.slice(0, 2).map((imageData, imgIndex) => {
+                    return (
+                      <TouchableOpacity
+                        key={imgIndex}
+                        style={{
+                          paddingBottom: 2,
+                          width: '100%',
+                        }}>
+                        <Image
+                          source={imageData}
+                          style={{width: '100%', height: 150}}
+                        />
+                      </TouchableOpacity>
+                    );
+                  })}
+                </View>
+              </View>
+            ) : null}
           </View>
         );
       })}
