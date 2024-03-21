e48a07dd2f983c45e11a2f76a94bbc129938f883
jhsonnn <jihyeongson13@gmail.com>
2024-03-21 14:11:06 +0900

    FriendProfile Screen ProfileBody 생성하기

---
+++ ReactNativeInstaApp/src/components/ProfileBody.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Image, Text, View} from 'react-native';
+
+const ProfileBody = ({name, profileImage, post, followers, following}) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingVertical: 20,
+      }}>
+      <View
+        style={{
+          alignItems: 'center',
+        }}>
+        <Image
+          source={profileImage}
+          style={{
+            width: 80,
+            height: 80,
+            borderRadius: 100,
+          }}
+        />
+        <Text style={{paddingVertical: 5, fontWeight: 'bold'}}>{name}</Text>
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontWeight: 'bold'}}>{post}</Text>
+        <Text>게시물</Text>
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontWeight: 'bold'}}>{followers}</Text>
+        <Text>팔로워</Text>
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontWeight: 'bold'}}>{following}</Text>
+        <Text>팔로잉</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ProfileBody;
--- ReactNativeInstaApp/src/screens/FriendProfile.js
+++ ReactNativeInstaApp/src/screens/FriendProfile.js
@@ -1,11 +1,55 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import ProfileBody from '../components/ProfileBody';
 
-const FriendProfile = () => {
+const FriendProfile = ({route, navigation}) => {
+  const {name, profileImage, post, followers, following} = route.params;
+
   return (
-    <View>
-      <Text>FriendProfile</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        width: '100%',
+        backgroundColor: 'white',
+      }}>
+      <View style={{padding: 10}}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionic
+              name="arrow-back"
+              style={{
+                fontSize: 20,
+                color: 'black',
+              }}
+            />
+          </TouchableOpacity>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '92%',
+            }}>
+            <Text style={{fontSize: 15, marginLeft: 10, fontWeight: 'bold'}}>
+              {name}
+            </Text>
+            <Feather
+              name="more-vertical"
+              style={{fontSize: 20, color: 'black'}}
+            />
+          </View>
+        </View>
+
+        <ProfileBody
+          name={name}
+          profileImage={profileImage}
+          post={post}
+          followers={followers}
+          following={following}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
