18474a134e3a1b575a9c9a7fba0f40cdcf3b7faa
jhsonnn <jihyeongson13@gmail.com>
2024-03-21 15:22:14 +0900

    Profile Screen UI 생성하기

--- ReactNativeInstaApp/src/components/ProfileBody.js
+++ ReactNativeInstaApp/src/components/ProfileBody.js
@@ -1,40 +1,93 @@
 import React from 'react';
 import {Image, Text, View} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 
-const ProfileBody = ({name, profileImage, post, followers, following}) => {
+const ProfileBody = ({
+  name,
+  profileImage,
+  post,
+  followers,
+  following,
+  accountName,
+}) => {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        paddingVertical: 20,
-      }}>
+    <View>
+      {accountName ? (
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: 'bold',
+              }}>
+              {accountName}
+            </Text>
+            <Feather
+              name="chevron-down"
+              style={{
+                fontSize: 20,
+                color: 'black',
+                paddingHorizontal: 5,
+                opacity: 0.5,
+              }}
+            />
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Feather
+              name="plus-square"
+              style={{
+                fontSize: 25,
+                color: 'black',
+                paddingHorizontal: 5,
+                opacity: 0.5,
+              }}
+            />
+            <Feather name="menu" style={{fontSize: 25}} />
+          </View>
+        </View>
+      ) : null}
       <View
         style={{
+          flexDirection: 'row',
           alignItems: 'center',
+          justifyContent: 'space-around',
+          paddingVertical: 20,
         }}>
-        <Image
-          source={profileImage}
+        <View
           style={{
-            width: 80,
-            height: 80,
-            borderRadius: 100,
-          }}
-        />
-        <Text style={{paddingVertical: 5, fontWeight: 'bold'}}>{name}</Text>
-      </View>
-      <View style={{alignItems: 'center'}}>
-        <Text style={{fontWeight: 'bold'}}>{post}</Text>
-        <Text>게시물</Text>
-      </View>
-      <View style={{alignItems: 'center'}}>
-        <Text style={{fontWeight: 'bold'}}>{followers}</Text>
-        <Text>팔로워</Text>
-      </View>
-      <View style={{alignItems: 'center'}}>
-        <Text style={{fontWeight: 'bold'}}>{following}</Text>
-        <Text>팔로잉</Text>
+            alignItems: 'center',
+          }}>
+          <Image
+            source={profileImage}
+            style={{
+              width: 80,
+              height: 80,
+              borderRadius: 100,
+            }}
+          />
+          <Text style={{paddingVertical: 5, fontWeight: 'bold'}}>{name}</Text>
+        </View>
+        <View style={{alignItems: 'center'}}>
+          <Text style={{fontWeight: 'bold'}}>{post}</Text>
+          <Text>게시물</Text>
+        </View>
+        <View style={{alignItems: 'center'}}>
+          <Text style={{fontWeight: 'bold'}}>{followers}</Text>
+          <Text>팔로워</Text>
+        </View>
+        <View style={{alignItems: 'center'}}>
+          <Text style={{fontWeight: 'bold'}}>{following}</Text>
+          <Text>팔로잉</Text>
+        </View>
       </View>
     </View>
   );
--- ReactNativeInstaApp/src/screens/Profile.js
+++ ReactNativeInstaApp/src/screens/Profile.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import ProfileBody from '../components/ProfileBody';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={{width: '100%', backgroundColor: 'white'}}>
+      <View style={{width: '100%', padding: 10}}>
+        <ProfileBody
+          name="Johnny"
+          accountName="johnny"
+          profileImage={require('../assets/images/userProfile.jpeg')}
+          followers="300"
+          following="35"
+          post="458"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
