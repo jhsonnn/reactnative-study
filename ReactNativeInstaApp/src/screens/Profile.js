269d1cb04d27a5ee6c0252cb7a147b4142e4949d
jhsonnn <jihyeongson13@gmail.com>
2024-03-21 15:50:39 +0900

    Profile Screen Circles 생성하기

--- ReactNativeInstaApp/src/screens/Profile.js
+++ ReactNativeInstaApp/src/screens/Profile.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
 import ProfileBody from '../components/ProfileBody';
 import ProfileButton from '../components/ProfileButton';
 
 const Profile = () => {
+  let circles = [];
+  let numberCircles = 10;
+
+  for (let index = 0; index < numberCircles; index++) {
+    circles.push(
+      <View key={index}>
+        {index === 0 ? (
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 100,
+              borderWidth: 1,
+              opacity: 0.7,
+              marginHorizontal: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Entypo
+              name="plus"
+              style={{
+                fontSize: 40,
+                color: 'black',
+              }}
+            />
+          </View>
+        ) : (
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 100,
+              backgroundColor: 'black',
+              opacity: 0.1,
+              marginHorizontal: 5,
+            }}
+          />
+        )}
+      </View>,
+    );
+  }
+
   return (
     <SafeAreaView style={{width: '100%', backgroundColor: 'white'}}>
       <View style={{width: '100%', padding: 10}}>
@@ -21,6 +64,17 @@ const Profile = () => {
           accountName="johnny"
           profileImage={require('../assets/images/userProfile.jpeg')}
         />
+      </View>
+      <View>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={{
+            paddingVertical: 5,
+            paddingHorizontal: 10,
+          }}>
+          {circles}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
